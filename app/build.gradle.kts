a4b5cc3f7997c7995e968cd6cb55ad1d9ff4ee7d
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-09-13 00:03:00 +0600

    lsst

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,13 +20,6 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            optimization {
-                enable = false
-            }
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
