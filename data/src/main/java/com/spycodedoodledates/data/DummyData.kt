6232bd5a0075c742fdae61cddcb8c6e83422c150
Rabbi Hossain <rabbihossainlhp@gmail.com>
2026-09-12 15:46:31 +0600

    re-structure project

--- data/src/main/java/com/spycodedoodledates/data/DummyData.kt
+++
@@ -1,3 +0,0 @@
-package com.spycodedoodledates.data
-
-class DummyData(val name: String)
--- domain/src/main/java/com/spycodedoodledates/domain/Dummy.kt
+++
@@ -1,5 +0,0 @@
-package com.spycodedoodledates.domain
-import kotlinx.serialization.Serializable
-
-@Serializable
-class Dummy(val id: Int)
